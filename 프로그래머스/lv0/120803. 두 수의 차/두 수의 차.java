b9304c916e4c8d8ac5b0efd2f26f2fd6a15c5ff0
Gyu Jun Kim <90171196+mbc2579@users.noreply.github.com>
2023-06-02 14:31:50 +0900

    [level 0] Title: 두 수의 차, Time: 0.26 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        
+        System.out.print(num1 - num2);
+        return answer;
+    }
+}
